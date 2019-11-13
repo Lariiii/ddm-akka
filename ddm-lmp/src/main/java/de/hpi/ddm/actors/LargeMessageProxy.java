f7634ead6eb7f1601990a5e766fe3a62c3d13b1f
Lariiii <lari-lari@gmx.net>
2019-11-13 11:37:15 +0100

    Add sending of byte batches.

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,14 +1,8 @@
 package de.hpi.ddm.actors;
 
 import java.io.*;
-import java.util.Arrays;
 
-import akka.NotUsed;
-import akka.actor.AbstractLoggingActor;
-import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
-import akka.actor.Props;
-import akka.stream.javadsl.Source;
+import akka.actor.*;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Output;
 import lombok.AllArgsConstructor;
@@ -42,6 +36,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	public static class BytesMessage<T> implements Serializable {
 		private static final long serialVersionUID = 4057807743872319842L;
 		private T bytes;
+		private int id;
 		private ActorRef sender;
 		private ActorRef receiver;
 	}
@@ -70,7 +65,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
+
 		// This will definitely fail in a distributed setting if the serialized message is large!
 		// Solution options:
 		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
@@ -78,20 +73,27 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		// 3. Send the object via Akka's http client-server component.
 		// 4. Other ideas ...
 
-        // convert message into byte array using kryo
-        byte[] byteMessage = convertByte(message.getMessage());
+		// serialize message into byte array using kryo
+		byte[] byteMessage = convertToBytes(message.getMessage());
 
-        // TODO: convert byteMessage into chunks (hardcode chunk sizes)
+		// convert serialized byteMessage into chunks with hardcoded size
+		// TODO: check if dynamically assigning a chunk size is better
+		byte[][] chunks = divideArray(byteMessage, 4096);
 
-        // create source
-        final Source<byte[], NotUsed> source =
-                Source.from(Arrays.asList(byteMessage));
-
-        receiverProxy.tell(new BytesMessage<>(source, this.sender(), message.getReceiver()), this.self());
+		for(int i=0; i < chunks.length; i++){
+			BytesMessage<byte[]> msg = new BytesMessage<>();
+			msg.bytes = chunks[i];
+			// TODO: check whether there is a better way to set the id for reassembling
+			msg.id = i;
+			//System.out.println(msg.id + " A " + msg.bytes);
+			msg.receiver = receiver;
+			msg.sender = this.sender();
+			receiverProxy.tell(msg, this.self());
+		}
 	}
 
-
-	private byte[] convertByte(Object message) {
+	// kryo documentation: https://github.com/EsotericSoftware/kryo
+	private byte[] convertToBytes(Object message) {
         try {
             Kryo kryo = new Kryo();
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -100,13 +102,35 @@ public class LargeMessageProxy extends AbstractLoggingActor {
             output.close();
             stream.close();
             return stream.toByteArray();
-        }catch(Exception e){
+        } catch (Exception e){
             return null;
         }
     }
 
+    // divide the byte message array into chunks
+	private static byte[][] divideArray(byte[] byteMessage, int chunksize) {
+		// TODO: check if byteMessage is completely divided into chunks
+		byte[][] result = new byte[((int)Math.ceil(byteMessage.length)/(int)chunksize)+1][chunksize];
+		int start = 0;
+
+		for(int i = 0; i < result.length; i++) {
+			if(start + chunksize > byteMessage.length) {
+				System.arraycopy(byteMessage, start, result[i], 0, byteMessage.length - start);
+			} else {
+				System.arraycopy(byteMessage, start, result[i], 0 , chunksize);
+			}
+			start += chunksize;
+		}
+
+		return result;
+	}
+
 	private void handle(BytesMessage<?> message) {
 		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+
+		// TODO: reassemble the chunks
+		//System.out.println(message.id + " B " + message.bytes);
         message.getReceiver().tell(message.getBytes(), message.getSender());
+
 	}
 }
